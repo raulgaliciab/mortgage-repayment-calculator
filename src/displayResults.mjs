5f66af5ac0f42543c7fa3fbb37148feb8362cfdc
Raul Galicia <raul.galiciab@gmail.com>
2024-11-13 22:06:36 -0600

    displayResults.mjs finished

---
+++ src/displayResults.mjs
@@ -0,0 +1,22 @@
+export function displayResults({monthlyPayment, totalPayment}) {
+
+  // QuerySelector para mostrar los datos
+
+const resultsMonthlyHTML = document.querySelector('#results-data__monthly');
+const resultsTotalHTML = document.querySelector('#results-data__total');
+
+// QuerySelector para el display
+
+const resultsEmptyDisplay = document.querySelector('#results__empty');
+const resultsFilledDisplay = document.querySelector('#results__filled');
+
+const resultsMonthlyStyled = parseFloat(monthlyPayment.toFixed(2)).toLocaleString("en-US");
+const resultsTotalStyled = parseFloat(totalPayment.toFixed(2)).toLocaleString("en-US");
+
+resultsMonthlyHTML.innerHTML = `£${resultsMonthlyStyled}`;
+resultsTotalHTML.innerHTML = `£${resultsTotalStyled}`;
+
+// Cambio de display
+resultsEmptyDisplay.style.display = "none";
+resultsFilledDisplay.style.display = "block";
+}
--- src/mortgageCalculator.mjs
+++ src/mortgageCalculator.mjs
@@ -1,3 +1,5 @@
+import { displayResults } from "./displayResults.mjs";
+
 export function calculateMortgage({ amount, term, interest_rate, mortgage_type }) {
   const monthlyRate = (interest_rate / 100) / 12;
   const totalNumberPayments = term * 12;
@@ -7,20 +9,24 @@ export function calculateMortgage({ amount, term, interest_rate, mortgage_type }
   function calculateRepayment() {
     const repaymentMonthly = amount * (monthlyRate * ((1 + monthlyRate) ** totalNumberPayments) / ((1 + monthlyRate) ** totalNumberPayments - 1));
     const repaymentTotal = repaymentMonthly * totalNumberPayments;
-    console.log(repaymentMonthly, repaymentTotal);
-    return {
+
+    const results = {
       monthlyPayment: repaymentMonthly,
       totalPayment: repaymentTotal
     };
+
+    displayResults(results);
   }
 
   function calculateInterestOnly() {
     const interestOnlyMonthly = amount * monthlyRate;
     const interestOnlyTotal = interestOnlyMonthly * totalNumberPayments;
-    console.log(interestOnlyMonthly, interestOnlyTotal);
-    return {
+
+    const results = {
       monthlyPayment: interestOnlyMonthly,
       totalPayment: interestOnlyTotal
     };
+
+    displayResults(results);
   }
 }
